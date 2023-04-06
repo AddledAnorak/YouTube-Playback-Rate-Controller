7db02da7880c160900b6afd43870a9469d466743
AddledAnorak <77722288+AddledAnorak@users.noreply.github.com>
2023-04-07 00:07:18 +0530

    step-by-step introduction of comms: popup + background now

--- background.js
+++ background.js
@@ -1,10 +1,12 @@
 chrome.runtime.onMessage.addListener((request, sender, senderResponse) => {
 
-    chrome.tabs.query({active: true, currentWindow: true}, tabs => {
-        chrome.tabs.sendMessage(tabs[0].id, {speed: request.speed}, response => {
-            console.log(response.confirmation + "by background");
-        });  
-    });
+    // chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+    //     chrome.tabs.sendMessage(tabs[0].id, {speed: request.speed}, response => {
+    //         console.log(response.confirmation + "by background");
+    //     });  
+    // });
+
+    console.log('will send msg sir!');
     
 
     senderResponse({
